6572f7743bc086963e54cfc5227d1b4bf280d92b
Abdaisy <abadsiyconceicao@epfundao.edu.pt>
2026-04-02 14:49:15 +0100

    resolver erro de botao mudar ink

---
+++ Migrations/20260402134834_AdicionarCampoRecuperacao.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestaopedagogica.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarCampoRecuperacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPlanoRecuperacao",
+                table: "Trabalhos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPlanoRecuperacao",
+                table: "Trabalhos");
+        }
+    }
+}
